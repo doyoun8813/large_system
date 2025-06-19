323d2cf9370b0d054f0fa5ea0f40d006d96d88fd
doyoun <doyoun8813@gmail.com>
2025-06-19 18:14:24 +0900

    캐시 최적화 전략 설계 - Request Collapsing

--- service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
+++ service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -1,6 +1,7 @@
 package kuke.board.articleread.client;
 
 import jakarta.annotation.PostConstruct;
+import kuke.board.articleread.cache.OptimizedCacheable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,7 +25,8 @@ public class ViewClient {
     // 레디스에서 데이터를 조회한다.
     // 레디스에 데이터가 없다면, count 메서드 내부 로직이 호출되면서, viewService로 원본 데이터를 요청한다. 그리고. 레디스에 데이터를 넣고 응답한다.
     // 레디스에 데이터가 있다면, 그 데이터를 그대로 반환한다.
-    @Cacheable(key = "#articleId", value = "articleViewCount")
+//    @Cacheable(key = "#articleId", value = "articleViewCount")
+    @OptimizedCacheable(type = "articleViewCount", ttlSeconds = 1)
     public long count(Long articleId) {
         log.info("[ViewClient.count] articleId={}", articleId);
         try {
